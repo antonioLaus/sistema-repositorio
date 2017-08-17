3d0b4fe64bc4a64530342811276f7f1b752537f4
Antonio Pedro Laus Neto <antoniopikadura007@gmail.com>
2017-08-17 17:22:31 -0300

    Desinvolvimento da classe Revista com os atributos

---
+++ src/model/revista.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ANTONIO PEDRO LAUS NETO 08/07/2017
+ */
+public class revista {
+    
+    private int codigo,paginas;
+    private String nome,descricao;
+    
+    
+}
